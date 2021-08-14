aaba341ce03e0f4f09f3b1c186e168452effea15
Devang Manjramkar <devangmanjramkar@gmail.com>
2021-08-15 02:03:52 +0530

    alll1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Services from './components/pages/Services';
+import Projects from './components/pages/Projects';
 import Gallary from './components/pages/Gallary';
 import SignUp from './components/pages/SignUp';
 import Icofont from 'react-icofont';
@@ -17,9 +17,9 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
+          <Route path='/' exact component={Home} /> 
           <Route path='/gallary' component={Gallary} />
+          <Route path='/projects' component={Projects} />
           <Route path='/sign-up' component={SignUp} />
         </Switch>
       </Router>
---
+++ src/components/pages/Projects.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "../../App.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
+export default function Services() {
+  return (
+    <div class="gallary">
+      <div>
+        <h1>My Certificates</h1>
+      </div>
+      <br />
+      <br />
+      <Carousel
+        infiniteLoop
+        autoPlay
+        stopOnHover
+        transitionTime="500"
+        
+      >
+        <a
+          href="https://github.com/DevangML/Advanced-Scientific-Calculator"
+          target="_blank"
+        >
+          <div>
+            <img src="./images/Proj/ASC.png" />
+            <p className="legend">Advanced Scientific Calculator</p>
+          </div>
+        </a>
+        <a
+          href="https://github.com/DevangML/DevangML.github.io"
+          target="_blank"
+        >
+          <div>
+            <img src="./images/Proj/WebSite.png" />
+            <p className="legend">Portfolio Website</p>
+          </div>
+        </a>
+        <a href="https://github.com/DevangML/Pandemic-Warrior" target="_blank">
+          <div>
+            <img src="./images/Proj/Pandemic.svg" />
+            <p className="legend">Pandemic Warrior</p>
+          </div>
+        </a>
+        <a href="https://github.com/DevangML/Phoenix-The-Virtual-Assistant" target="_blank">
+          <div>
+            <img src="./images/Proj/Phoenix.svg" />
+            <p className="legend">Phoenix-The Virtual Assistant</p>
+          </div>
+        </a>
+      </Carousel>
+    </div>
+  );
+}
