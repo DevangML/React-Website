5fdfdc704379010b8f57b8dd1ffa4ae0b405fe15
Devang Manjramkar <devangmanjramkar@gmail.com>
2021-08-15 01:35:10 +0530

    alll

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
-import Projects from './components/pages/Projects';
+import Gallary from './components/pages/Gallary';
 import SignUp from './components/pages/SignUp';
 import Icofont from 'react-icofont';
 
@@ -19,7 +19,7 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/services' component={Services} />
-          <Route path='/projects' component={Projects} />
+          <Route path='/gallary' component={Gallary} />
           <Route path='/sign-up' component={SignUp} />
         </Switch>
       </Router>
---
+++ src/components/pages/Gallary.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "../../App.css";
+import ReactDOM from "react-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
+export default function Gallary() {
+  return (
+    <div class="gallary">
+      <div>
+        <h1 className='H1'>My Certificates</h1>
+      </div>
+      <Carousel infiniteLoop autoPlay stopOnHover transitionTime='500' className='Car'>
+        <div>
+          <img src="./images/Cert/AI.jpeg" />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <img src="./images/Cert/C.jpeg" />
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <img src="./images/Cert/C++.jpeg" />
+          <p className="legend">Legend 3</p>
+        </div>
+        <div>
+          <img src="./images/Cert/C++_DSC.jpeg" />
+          <p className="legend">Legend 3</p>
+        </div>
+        <div>
+          <img src="./images/Cert/EF-SET.jpeg" />
+          <p className="legend">Legend 3</p>
+        </div>
+        <div>
+          <img src="./images/Cert/ISA-C.jpeg" />
+          <p className="legend">Legend 3</p>
+        </div>
+        <div>
+          <img src="./images/Cert/Python-Beg.jpeg" />
+          <p className="legend">Legend 3</p>
+        </div>
+        <div>
+          <img src="./images/Cert/Python-IEEE.jpeg" />
+          <p className="legend">Legend 3</p>
+        </div>
+        <div>
+          <img src="./images/Cert/Python-Int.jpeg" />
+          <p className="legend">Legend 3</p>
+        </div>
+        <div>
+          <img src="./images/Cert/VishwaConclave.jpeg" />
+          <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
+    </div>
+  );
+}
