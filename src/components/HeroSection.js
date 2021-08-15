472c62bba9f440d57949f120efb40ff520541451
Devang <devangmanjramkar@gmail.com>
2021-08-15 22:13:19 +0530

    ds

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -11,9 +11,8 @@ function HeroSection() {
     <div className='hero-container'>
       
       <video src='/videos/video-1.mp4' autoPlay loop muted />
-      
-      <h1>Devang Manjramkar</h1>
-      <Typed strings={['I am Web Developer']} typeSpeed={44} loop style={{color:'whitesmoke'}}/>
+      <h1 className='H2'>Devang Manjramkar</h1>
+      <Typed strings={['I am Web Developer']} typeSpeed={44} loop className='type'/>
       <div className='hero-btns'>
         <Button
           className='btns'
