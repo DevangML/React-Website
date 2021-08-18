99aa417a99a27a0f666982fbb47d2178d2a0bfa8
Devang Manjramkar <devangmanjramkar@gmail.com>
2021-08-18 13:23:47 +0530

    d

--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -2,31 +2,36 @@ import React from 'react';
 import '../../App.css';
 
 export default function ContactMe() {
+  const FacebookBackground =
+    "linear-gradient(to right, #0546A0 0%, #0546A0 40%, #663FB6 100%)";
+  const InstagramBackground =
+    "linear-gradient(to right, #A12AC4 0%, #ED586C 40%, #F0A853 100%)";
+  const TwitterBackground =
+    "linear-gradient(to right, #56C1E1 0%, #35A9CE 50%)";
   return (
-    <div className='contact-container'>
-      <div className='form-cont'>
-        <form>
-          <div>
-            <label htmlFor="name">Name:</label>
-            &nbsp;&nbsp;
-            <input type="text" id="name" required />
-          </div>
-          <br/>
-          <div>
-            <label htmlFor="email">Email:</label>
-            &nbsp;&nbsp;
-            <input type="email" id="email" required />
-          </div>
-          <br/>
-          <div>
-            <label htmlFor="message">Message:</label>
-            &nbsp;&nbsp;
-            <textarea id="message" required />
-          </div>
-          <br/>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    </div>
+    <MainContainer>
+    <WelcomeText>Welcome</WelcomeText>
+    <InputContainer>
+      <Input type="text" placeholder="Email" />
+      <Input type="password" placeholder="Password" />
+    </InputContainer>
+    <ButtonContainer>
+      <Button content="Sign Up" />
+    </ButtonContainer>
+    <LoginWith>OR LOGIN WITH</LoginWith>
+    <HorizontalRule />
+    <IconsContainer>
+      <Icon color={FacebookBackground}>
+        <FaFacebookF />
+      </Icon>
+      <Icon color={InstagramBackground}>
+        <FaInstagram />
+      </Icon>
+      <Icon color={TwitterBackground}>
+        <FaTwitter />
+      </Icon>
+    </IconsContainer>
+    <ForgotPassword>Forgot Password ?</ForgotPassword>
+  </MainContainer>
   );
 }
