a44b4a07bf0c7491085e1285efcb83461a60eb44
Devang Manjramkar <devangmanjramkar@gmail.com>
2021-08-20 00:41:49 +0530

    sdd

--- src/components/pages/Timeline.js
+++ src/components/pages/Timeline.js
@@ -1,106 +1,109 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Timeline from '@material-ui/lab/Timeline';
-import TimelineItem from '@material-ui/lab/TimelineItem';
-import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
-import TimelineConnector from '@material-ui/lab/TimelineConnector';
-import TimelineContent from '@material-ui/lab/TimelineContent';
-import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
-import TimelineDot from '@material-ui/lab/TimelineDot';
-import FastfoodIcon from '@material-ui/icons/Fastfood';
-import LaptopMacIcon from '@material-ui/icons/LaptopMac';
-import HotelIcon from '@material-ui/icons/Hotel';
-import RepeatIcon from '@material-ui/icons/Repeat';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    padding: '6px 16px',
-  },
-  secondaryTail: {
-    backgroundColor: theme.palette.secondary.main,
-  },
-}));
-
-export default function CustomizedTimeline() {
-  const classes = useStyles();
-
+function Timeline() {
   return (
-    <Timeline align="alternate" className='tim-cont'>
-      <TimelineItem>
-        <TimelineOppositeContent>
-          <Typography variant="body2" color="textSecondary">
-            9:30 am
-          </Typography>
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot>
-            <FastfoodIcon />
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Paper elevation={3} className={classes.paper}>
-            <Typography variant="h6" component="h1">
-              Eat
-            </Typography>
-            <Typography>Because you need strength</Typography>
-          </Paper>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent>
-          <Typography variant="body2" color="textSecondary">
-            10:00 am
-          </Typography>
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot color="primary">
-            <LaptopMacIcon />
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Paper elevation={3} className={classes.paper}>
-            <Typography variant="h6" component="h1">
-              Code
-            </Typography>
-            <Typography>Because it&apos;s awesome!</Typography>
-          </Paper>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot color="primary" variant="outlined">
-            <HotelIcon />
-          </TimelineDot>
-          <TimelineConnector className={classes.secondaryTail} />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Paper elevation={3} className={classes.paper}>
-            <Typography variant="h6" component="h1">
-              Sleep
-            </Typography>
-            <Typography>Because you need rest</Typography>
-          </Paper>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot color="secondary">
-            <RepeatIcon />
-          </TimelineDot>
-        </TimelineSeparator>
-        <TimelineContent>
-          <Paper elevation={3} className={classes.paper}>
-            <Typography variant="h6" component="h1">
-              Repeat
-            </Typography>
-            <Typography>Because this is the life you love!</Typography>
-          </Paper>
-        </TimelineContent>
-      </TimelineItem>
-    </Timeline>
+    <div className='tim-cont'>
+      <section className="timeline">
+        <h1 className='tim-h1'>Timeline of my life</h1>
+        <div className="demo-card-wrapper">
+          <div className="demo-card demo-card--step1">
+            <div className="head">
+              <div className="number-box">
+                <span>01</span>
+              </div>
+              <h2>
+                <span className="small">Subtitle</span> Technology
+              </h2>
+            </div>
+            <div className="body">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
+                reiciendis deserunt doloribus consequatur, laudantium odio
+                dolorum laboriosam.
+              </p>
+              <img src="http://placehold.it/1000x500" alt="Graphic" />
+            </div>
+          </div>
+
+          <div className="demo-card demo-card--step2">
+            <div className="head">
+              <div className="number-box">
+                <span>02</span>
+              </div>
+              <h2>
+                <span className="small">Subtitle</span> Confidence
+              </h2>
+            </div>
+            <div className="body">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
+                reiciendis deserunt doloribus consequatur, laudantium odio
+                dolorum laboriosam.
+              </p>
+              <img src="http://placehold.it/1000x500" alt="Graphic" />
+            </div>
+          </div>
+
+          <div className="demo-card demo-card--step3">
+            <div className="head">
+              <div className="number-box">
+                <span>03</span>
+              </div>
+              <h2>
+                <span className="small">Subtitle</span> Adaptation
+              </h2>
+            </div>
+            <div className="body">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
+                reiciendis deserunt doloribus consequatur, laudantium odio
+                dolorum laboriosam.
+              </p>
+              <img src="http://placehold.it/1000x500" alt="Graphic" />
+            </div>
+          </div>
+
+          <div className="demo-card demo-card--step4">
+            <div className="head">
+              <div className="number-box">
+                <span>04</span>
+              </div>
+              <h2>
+                <span className="small">Subtitle</span> Consistency
+              </h2>
+            </div>
+            <div className="body">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
+                reiciendis deserunt doloribus consequatur, laudantium odio
+                dolorum laboriosam.
+              </p>
+              <img src="http://placehold.it/1000x500" alt="Graphic" />
+            </div>
+          </div>
+
+          <div className="demo-card demo-card--step5">
+            <div className="head">
+              <div className="number-box">
+                <span>05</span>
+              </div>
+              <h2>
+                <span className="small">Subtitle</span> Conversion
+              </h2>
+            </div>
+            <div className="body">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
+                reiciendis deserunt doloribus consequatur, laudantium odio
+                dolorum laboriosam.
+              </p>
+              <img src="http://placehold.it/1000x500" alt="Graphic" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
+
+export default Timeline;
