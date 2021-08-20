69fe125ab243e5b82abae40b59ac7176a808d07b
Devang <devangmanjramkar@gmail.com>
2021-08-20 20:29:57 +0530

    neww

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from './Button';
+import { Link } from 'react-router-dom';
 import Typed from 'react-typed';
 
 
@@ -7,19 +7,12 @@ import Typed from 'react-typed';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <br/><br/><br/><br/><br/>
+      <br /><br /><br /><br /><br />
       <h1 className='Hero-H2'>Devang Manjramkar</h1>
       <br />
       <Typed strings={['I am Web Developer']} typeSpeed={44} loop className='type' />
       <div className='hero-btns'>
-        <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
-        >
-          Know More about Me <i className='far fa-play-circle' />
-        </Button>
+        <a href='https://drive.google.com/uc?export=download&id=1lcGTUHwzRJJPbqdlv5BwNfPQ8I2ZH-wr' target='_blank' className='hero-anchor'>Resume</a>
       </div>
     </div>
   );
