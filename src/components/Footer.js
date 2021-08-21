55ca2f9a364722ad21680c634b1f878cbcb07c3a
Devang Manjramkar <devangmanjramkar@gmail.com>
2021-08-21 16:47:44 +0530

    fghndfghfjh

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,18 @@
 import React from "react";
-import VIT from "../components/pages/image/2.jpg";
+import VIT from '../components/pages/image/VIT.svg'
 
 function Footer() {
   return (
     <div className="Foot">
       <h1 className="retroshadow foot-h1">I Study At</h1>
-      <div className="foot-img-cont">
-        <img className="foot-img" src={VIT} />
-      </div>
-      <h1 className="retroshadow foot-h1">Work At</h1>
-      <div className="foot-img-cont">
-        <img className="foot-img" src={VIT} />
-      </div>
+      <br/>
+        <img src={VIT} className='foot-img'/><br/>
+        <figcaption>Vishwakarma Institute of Technology</figcaption>
+        <h1 className="retroshadow foot-h1">Work At</h1>
+      <br/>
+        <img src={VIT} className='foot-img'/><br/>
+        <figcaption>IEEE Student's branch, VIT Pune</figcaption>
+
     </div>
   );
 }
