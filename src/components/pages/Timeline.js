1a986e1431bdb34a9565a24934bc370035245008
Devang Manjramkar <devangmanjramkar@gmail.com>
2021-08-22 15:36:04 +0530

    vbncvbn

--- src/components/pages/Timeline.js
+++ src/components/pages/Timeline.js
@@ -9,105 +9,105 @@ function Timeline() {
   }, []);
   return (
     <div className="tim-cont">
-      <section className="timeline">
-        <div className='tim-head'>Timeline Of My Life</div>
-        <div className="demo-card-wrapper">
-          <div className="demo-card demo-card--step1" data-aos="fade-right">
-            <div className="head">
-              <div className="number-box">
-                <span>01</span>
-              </div>
-              <h2>
-                <span className="small">Subtitle</span> Technology
-              </h2>
-            </div>
-            <div className="body">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-                reiciendis deserunt doloribus consequatur, laudantium odio
-                dolorum laboriosam.
-              </p>
-              <img src="http://placehold.it/1000x500" alt="Graphic" />
-            </div>
+      <div className="tim-head">Timeline Of My Life</div>
+      <div class="section agenda">
+        <div class="row column text-center">
+          <div class="agenda__month">
+            <h2 class="agenda__month__title">
+              <time datetime="">Sept 2016</time>
+            </h2>
+            <ol class="agenda__list">
+              <li class="agenda__item toiletteur">
+                <article class="agenda__item__article">
+                  <span class="agenda__item__date">12</span>
+                  <div class="agenda__item__detail">
+                    <div class="horaires">
+                      <time datetime="">9:15</time>
+                    </div>
+                    <div class="desc">
+                      <h3 class="agenda__item__title">Agenda item title</h3>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit.
+                      </p>
+                    </div>
+                    <button class="edit__btn">
+                      <i class="fa fa-pencil"></i>
+                    </button>
+                  </div>
+                </article>
+              </li>
+              <li class="agenda__item">
+                <article class="agenda__item__article">
+                  <span class="agenda__item__date">12</span>
+                  <div class="agenda__item__detail">
+                    <div class="horaires">
+                      <time datetime="">9:15</time>
+                    </div>
+                    <div class="desc">
+                      <h3 class="agenda__item__title">Agenda item title</h3>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit.
+                      </p>
+                    </div>
+                    <button class="edit__btn">
+                      <i class="fa fa-pencil"></i>
+                    </button>
+                  </div>
+                </article>
+              </li>
+            </ol>
           </div>
-
-          <div className="demo-card demo-card--step2" data-aos="fade-left">
-            <div className="head">
-              <div className="number-box">
-                <span>02</span>
-              </div>
-              <h2>
-                <span className="small">Subtitle</span> Confidence
-              </h2>
-            </div>
-            <div className="body">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-                reiciendis deserunt doloribus consequatur, laudantium odio
-                dolorum laboriosam.
-              </p>
-              <img src="http://placehold.it/1000x500" alt="Graphic" />
-            </div>
-          </div>
-
-          <div className="demo-card demo-card--step3" data-aos="fade-right">
-            <div className="head">
-              <div className="number-box">
-                <span>03</span>
-              </div>
-              <h2>
-                <span className="small">Subtitle</span> Adaptation
-              </h2>
-            </div>
-            <div className="body">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-                reiciendis deserunt doloribus consequatur, laudantium odio
-                dolorum laboriosam.
-              </p>
-              <img src="http://placehold.it/1000x500" alt="Graphic" />
-            </div>
-          </div>
-
-          <div className="demo-card demo-card--step4" data-aos="fade-left">
-            <div className="head">
-              <div className="number-box">
-                <span>04</span>
-              </div>
-              <h2>
-                <span className="small">Subtitle</span> Consistency
-              </h2>
-            </div>
-            <div className="body">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-                reiciendis deserunt doloribus consequatur, laudantium odio
-                dolorum laboriosam.
-              </p>
-              <img src="http://placehold.it/1000x500" alt="Graphic" />
-            </div>
-          </div>
-
-          <div className="demo-card demo-card--step5" data-aos="fade-right">
-            <div className="head">
-              <div className="number-box">
-                <span>05</span>
-              </div>
-              <h2>
-                <span className="small">Subtitle</span> Conversion
-              </h2>
-            </div>
-            <div className="body">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-                reiciendis deserunt doloribus consequatur, laudantium odio
-                dolorum laboriosam.
-              </p>
-              <img src="http://placehold.it/1000x500" alt="Graphic" />
-            </div>
+          <div class="agenda__month">
+            <h2 class="agenda__month__title">
+              <time datetime="">Oct 2016</time>
+            </h2>
+            <ol class="agenda__list">
+              <li class="agenda__item veterinaire">
+                <article class="agenda__item__article">
+                  <span class="agenda__item__date">12</span>
+                  <div class="agenda__item__detail">
+                    <div class="horaires">
+                      <time datetime="">9:15</time>
+                    </div>
+                    <div class="desc">
+                      <h3 class="agenda__item__title">Agenda item title</h3>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit.
+                      </p>
+                    </div>
+                    <button class="edit__btn">
+                      <i class="fa fa-pencil"></i>
+                    </button>
+                  </div>
+                </article>
+              </li>
+              <li class="agenda__item club">
+                <article class="agenda__item__article">
+                  <span class="agenda__item__date">12</span>
+                  <div class="agenda__item__detail">
+                    <div class="horaires">
+                      <time datetime="">9:15</time>
+                    </div>
+                    <div class="desc">
+                      <h3 class="agenda__item__title">Agenda item title</h3>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit.
+                      </p>
+                    </div>
+                    <button class="edit__btn">
+                      <i class="fa fa-pencil"></i>
+                    </button>
+                  </div>
+                </article>
+              </li>
+            </ol>
           </div>
         </div>
-      </section>
+      </div>
     </div>
   );
 }
