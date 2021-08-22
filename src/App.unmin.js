5ed50fa8bbccc15a6fb23058e13a2e1e881f5153
Devang Manjramkar <devangmanjramkar@gmail.com>
2021-08-23 01:08:36 +0530

    dfwqewef

---
+++ src/App.unmin.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.min.css';import Home from './components/pages/Home';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import Projects from './components/pages/Projects';
+import Gallery from './components/pages/Gallery';
+import Timedline from './components/pages/Timeline';
+import ContactMe from './components/pages/ContactMe'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+      <Route path='/gallery' component={Gallery} />
+      <Route path='/projects' component={Projects} />
+      <Route path='/timeline' component={Timedline} />
+      <Route path='/contactme' component={ContactMe} />
+      </Switch>
+    </Router >
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-
-
-ReactDOM.hydrate(<App/>, document.getElementById('root'));
+import React from 'react';import ReactDOM from 'react-dom';import App from './App';ReactDOM.hydrate(<App/>, document.getElementById('root'));
