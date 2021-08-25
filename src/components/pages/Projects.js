2b35cf1b5e7f054c5ab654a4a37327a1a8bd92af
Devang Manjramkar <devangmanjramkar@gmail.com>
2021-08-26 00:48:36 +0530

    New

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,64 +4,85 @@ import "aos/dist/aos.css";
 import "../../App.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-import ASC from '../../assets/images/Proj/ASC.png'
-import Pandemic from '../../assets/images/Proj/Pandemic.jpg'
-import Phoenix from '../../assets/images/Proj/Phoenix.png'
-import WebSite from '../../assets/images/Proj/WebSite.png'
+import ASC from "../../assets/images/Proj/ASC.png";
+import Pandemic from "../../assets/images/Proj/Pandemic.jpg";
+import Phoenix from "../../assets/images/Proj/Phoenix.png";
+import WebSite from "../../assets/images/Proj/WebSite.png";
 
 export default function Services() {
   useEffect(() => {
-    AOS.init({duration : 600});
+    AOS.init({ duration: 600 });
     AOS.refresh();
   }, []);
   return (
-    <div className="projects" data-aos="zoom-out" data-aos-easing="ease-out-cubic">
-      <div >
-      <h1 className='retroshadow proj-h1'>My Projects</h1>
+    <div
+      className="projects"
+      data-aos="zoom-out"
+      data-aos-easing="ease-out-cubic"
+    >
+      <div>
+        <h1 className="retroshadow proj-h1">My Projects</h1>
       </div>
-      
+
       <br />
       <br />
-      <Carousel
-        infiniteLoop
-        autoPlay
-        stopOnHover
-        transitionTime="500"
-        className='carr'
-        showStatus={false}
-        
-      >
-        <a
-          href="https://github.com/DevangML/Advanced-Scientific-Calculator"
-          target="_blank"
-        >
-          <div>
-            <img src={ASC} />
-            <p className="legend">Advanced Scientific Calculator</p>
-          </div>
-        </a>
-        <a
-          href="https://github.com/DevangML/DevangML.github.io"
-          target="_blank"
-        >
-          <div>
-            <img src={Pandemic} />
-            <p className="legend">Portfolio Website</p>
-          </div>
-        </a>
-        <a href="https://github.com/DevangML/Pandemic-Warrior" target="_blank">
-          <div>
-            <img src={Phoenix}/>
-            <p className="legend">Pandemic Warrior</p>
-          </div>
-        </a>
-        <a href="https://github.com/DevangML/Phoenix-The-Virtual-Assistant" target="_blank">
-          <div>
-            <img src={WebSite} />
-            <p className="legend">Phoenix-The Virtual Assistant</p>
-          </div>
-        </a>
-      </Carousel>
+      <div className="pcrdcont">
+        <div className="pcrd">
+          <a
+            href="https://github.com/DevangML/Advanced-Scientific-Calculator"
+            target="_blank"
+            className="PA"
+            data-aos="fade-right"
+            data-aos-anchor="#example-anchor"
+            data-aos-offset="500"
+            data-aos-duration="500"
+          >
+            <img src={ASC} className="PImg" />
+            <figcaption className='Pfigcapt'>Advanced Scientific Calculator</figcaption>
+          </a>
+          <a
+            href="https://github.com/DevangML/DevangML.github.io"
+            target="_blank"
+            className="PA"
+            data-aos="fade-left"
+            data-aos-anchor="#example-anchor"
+            data-aos-offset="500"
+            data-aos-duration="500"
+          >
+            <img src={Pandemic} className="PImg" />
+            <figcaption className='Pfigcapt'>Website</figcaption>
+          </a>
+        </div>
+        <br />
+        <br />
+        <br />
+        <div className="pcrd">
+          <a
+            href="https://github.com/DevangML/Pandemic-Warrior"
+            target="_blank"
+            className="PA"
+            data-aos="fade-right"
+            data-aos-anchor="#example-anchor"
+            data-aos-offset="500"
+            data-aos-duration="500"
+          >
+            <img src={Phoenix} className="PImg" />
+            <figcaption className='Pfigcapt'>Pandemic Warrior</figcaption>
+          </a>
+          <a
+            href="https://github.com/DevangML/Phoenix-The-Virtual-Assistant"
+            target="_blank"
+            className="PA"
+            data-aos="fade-left"
+            data-aos-anchor="#example-anchor"
+            data-aos-offset="500"
+            data-aos-duration="500"
+          >
+            <img src={WebSite} className="PImg" />
+            <figcaption className='Pfigcapt'>Phoenix-The-Virtual-Assistant</figcaption>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
