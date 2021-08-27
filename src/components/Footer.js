0421a48f1defc4e90dc744e58e313c7885a7d12a
Devang Manjramkar <devangmanjramkar@gmail.com>
2021-08-27 15:25:40 +0530

    nb

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,23 +12,23 @@ function Footer() {
 
   return (
     <section className="Foot">
-      <article>
-      <div className="tim-head">I Study At</div>
+      <article data-aos="fade-up">
+      <div className="tim-head" >I Study At</div>
       </article>
       <br />
-      <img src={VIT} className="foot-img" data-aos="slide-up" />
+      <img src={VIT} className="foot-img" data-aos="fade-up" />
       <br />
-      <figcaption data-aos="slide-up">
+      <figcaption data-aos="fade-up">
         Vishwakarma Institute of Technology
       </figcaption>
       <br />
       <br />
-      <article>
+      <article data-aos="fade-up"> 
         <div className="tim-head">Work At</div>
       </article>
       <br />
       <video
-        data-aos="slide-up"
+        data-aos="fade-up"
         src={IEEE}
         className="foot-img"
         autoPlay
